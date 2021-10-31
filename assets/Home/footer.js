c5259e5c3cc29d22a07e3d3da633c25a17016fe2
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2021-10-31 18:27:21 +0000

    Added More Detailed Comments

--- assets/Home/footer.js
+++ assets/Home/footer.js
@@ -1,12 +1,25 @@
+//Javascript file containing the logic for hiding and showing
+//the page footer when the mouse is near the bottom of the page
+
+//ISSUE: I am aware that this design doesn't work at all on
+//mobile, but as this is just a bit of fun I didn't consider
+//it to be a deal-breaker
+
+//Whenever the mouse is moved...
 document.addEventListener('mousemove', (event) => {
+  //Get the dimentions of the page
   let height = window.innerHeight
   let mouseY = event.clientY;
 
+  //heightDelta is the distance the mouse is from the bottom of the screen
   let heightDelta = height - mouseY;
+  //heightThreshold is how close to the bottom of the screen the mouse needs to be to
+  //activate the footer
   let heightThreshold = height * 0.16;
-  console.log(height + " " + mouseY);
   
+  //Get the footer
   let footer = document.getElementById("footer");
+  //If the mouse is below the threshold for activating the footer
   if (heightDelta < heightThreshold) {
     //Activate Footer
     footer.classList.add("grow"); //Note: Handles if the class is already there
--- assets/Home/opacity.js
+++ assets/Home/opacity.js
@@ -1,10 +1,18 @@
+//Javascript file containing the logic for dynamically changing the opacity
+//of each wonder section as you mouse over it
+
+//When the page is first loaded
 window.onload = () => {
+  //Get the wonders container
   let wonders = document.getElementsByClassName("wonder");
+  //And for every wonder in this container
   for (let i = 0; i < wonders.length; i++) {
     const wonder = wonders[i];
+    //When you put the mouse over it make it brighter
     wonder.addEventListener('mouseover', (event) => {
       wonder.classList.remove("darken");
     });
+    //When you remove the mouse from it make it darker
     wonder.addEventListener('mouseout', (event) => {
       wonder.classList.add("darken");
     });
